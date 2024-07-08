56947d027d2da5fec242c0a3a4e7517a11a77db9
Christian-Encalada <cris_7715@hotmail.com>
2024-07-08 18:56:24 -0500

    upd

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/reports', reportLabRoutes);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
