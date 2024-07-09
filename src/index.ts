5317eb8b4ac2ca2cbffc07466ee18818ffb61a3c
Christian-Encalada <cris_7715@hotmail.com>
2024-07-09 02:19:06 -0500

    creacion de plantilla worked

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import cors from 'cors';
+
 import userRoutes from './routes/users.route';
 import reportLabRoutes from './routes/reportLab.route';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSetup from './swagger';
 
 const app = express();
+const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +16,11 @@ app.use('/users', userRoutes)
 
 app.use('/reports', reportLabRoutes);
 app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerSetup));
+app.use(cors({
+  origin: 'http://localhost:3000', // Cambia esto a la URL de tu frontend si es diferente
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 
 const PORT = process.env.PORT || 3000;
