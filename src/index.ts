97a50a1fa4f81a0796bc84bc1370c513df0578a7
Christian-Encalada <cris_7715@hotmail.com>
2024-07-09 01:12:57 -0500

    documentation

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,20 @@ import express from 'express';
 import cors from 'cors';
 import userRoutes from './routes/users.route';
 import reportLabRoutes from './routes/reportLab.route';
+import swaggerUi from 'swagger-ui-express';
+import swaggerSetup from './swagger';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/users', userRoutes);
+app.use('/users', userRoutes)}
+
+
 app.use('/reports', reportLabRoutes);
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerSetup));
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
